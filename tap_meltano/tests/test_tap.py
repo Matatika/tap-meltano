7a2781f3ccd258b30993b5e46b0fe8aacd5c397a
danielpdwalker <danielpdwalker@gmail.com>
2021-12-09 13:54:17 +0000

    Added initial test for the tap

---
+++ tap_meltano/tests/test_tap.py
@@ -0,0 +1,31 @@
+"""Tests the tap against a test database"""
+
+import unittest
+import singer
+import os
+
+from tap_meltano.tap import TapMeltano
+
+SINGER_MESSAGES = []
+
+
+def accumulate_singer_messages(message):
+    """function to collect singer library write_message in tests"""
+    SINGER_MESSAGES.append(message)
+
+
+class TestTapMeltano(unittest.TestCase):
+    def setUp(self):
+        db = os.path.abspath("tap_meltano/tests/example_database/meltano.db")
+        db = f"sqlite:///{db}"
+        self.mock_config = {"meltano_database_uri": db}
+
+    del SINGER_MESSAGES[:]
+    singer.write_message = accumulate_singer_messages
+
+    def test_tap(self):
+        tap = TapMeltano(config=self.mock_config)
+
+        tap.sync_all()
+
+        self.assertEqual(len(SINGER_MESSAGES), 11)
