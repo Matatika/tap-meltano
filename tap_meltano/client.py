eb40191504d3464e6abac739278da6d2c5f85783
Reuben Frankel <rfrankel@matatika.com>
2025-01-18 00:07:30 +0000

    Prevent unnecessary schema prefix for stream names

--- tap_meltano/client.py
+++ tap_meltano/client.py
@@ -6,6 +6,7 @@ This includes MeltanoStream and MeltanoConnector.
 from __future__ import annotations
 
 from singer_sdk import SQLConnector, SQLStream
+from typing_extensions import override
 
 
 class MeltanoConnector(SQLConnector):
@@ -13,6 +14,30 @@ class MeltanoConnector(SQLConnector):
 
     def get_sqlalchemy_url(self, config):
         return config["meltano_database_uri"]
+
+    @override
+    def discover_catalog_entry(
+        self,
+        engine,
+        inspected,
+        schema_name,
+        table_name,
+        is_view,
+        *,
+        reflected_columns=None,
+        reflected_pk=None,
+        reflected_indices=None,
+    ):
+        return super().discover_catalog_entry(
+            engine,
+            inspected,
+            None,  # prevent unnecessary schema prefix, as everything should only ever be in a single schema
+            table_name,
+            is_view,
+            reflected_columns=reflected_columns,
+            reflected_pk=reflected_pk,
+            reflected_indices=reflected_indices,
+        )
 
 
 class MeltanoStream(SQLStream):
